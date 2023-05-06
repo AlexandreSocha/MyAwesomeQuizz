94b9d61086da1c2cbbc575b49d3ef25081cef8fd
alexa <alexandre.socha1994@gmail.com>
2023-05-06 18:54:02 +0200

    Spinner et difficultés

--- app/src/main/java/com/alexandresocha/myawesomequizz/StartingScreenActivity.java
+++ app/src/main/java/com/alexandresocha/myawesomequizz/StartingScreenActivity.java
@@ -7,17 +7,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class StartingScreenActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_QUIZ = 1;
+    public static final String EXTRA_DIFFICULTY = "extraDifficulty";
     private static final String SHARED_PREFS = "sharedPrefs";
     private static final String KEY_HIGHSCORE = "keyHighscore";
     private TextView textViewHighscore;
+    private Spinner spinnerDifficulty;
 
     private int highscore;
+
+
 
 
     @Override
@@ -26,6 +32,15 @@ public class StartingScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starting_screen);
 
         textViewHighscore = findViewById(R.id.text_view_highscore);
+        spinnerDifficulty = findViewById(R.id.spinner_difficulty);
+
+        String[] difficultyLevels = Question.getAllDifficultyLevels();
+
+        ArrayAdapter<String> adapterDifficulty = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, difficultyLevels);
+        adapterDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDifficulty.setAdapter(adapterDifficulty);
+
         loadHighscore();
 
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
@@ -38,7 +53,9 @@ public class StartingScreenActivity extends AppCompatActivity {
     }
 
     private void startQuiz(){
+        String difficulty = spinnerDifficulty.getSelectedItem().toString();
         Intent intent = new Intent(StartingScreenActivity.this, QuizzActivity.class);
+        intent.putExtra(EXTRA_DIFFICULTY, difficulty);
         startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
 
